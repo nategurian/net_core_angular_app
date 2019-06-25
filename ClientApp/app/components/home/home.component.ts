2c7a0886e76ec43aedd28a04cad7664222f552ae
Nate Gurian <nategurian@gmail.com>
2019-06-24 21:14:45 -0400

    added search feature

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Observable, BehaviorSubject } from "rxjs";
 import { Component, OnInit } from "@angular/core";
 import { DataService } from "./../../services/dataservice";
 import { Story } from "../../Models/story.model";
@@ -10,10 +11,13 @@ import { Story } from "../../Models/story.model";
 export class HomeComponent implements OnInit {
   loading: boolean;
   stories: Story[];
+  filter: string;
+  filteredStories: BehaviorSubject<Story[]> = new BehaviorSubject<Story[]>([]);
 
   constructor(private ds: DataService) {
     this.loading = true;
     this.stories = [];
+    this.filter = "";
   }
 
   ngOnInit() {
@@ -25,6 +29,7 @@ export class HomeComponent implements OnInit {
     if (savedStories) {
       this.stories = JSON.parse(savedStories);
       this.loading = false;
+      this.filteredStories.next(this.stories);
     } else {
       this.ds.getBestStories().then(data => {
         let response: any = data;
@@ -49,7 +54,34 @@ export class HomeComponent implements OnInit {
           localStorage.setItem("stories", JSON.stringify(this.stories));
         }
         this.loading = false;
+        this.filteredStories.next(this.stories);
       });
+    }
+  }
+
+  onFilterChange(event: any) {
+    let temp: Story[] = [];
+    // if filter is an empty string assume its a clear and add
+    // all stories to the filtered stories
+    if (event === "") {
+      this.filteredStories.next(this.stories);
+      this.filter = "";
+    }
+    // filter the stories based on the incoming input value
+    // filter checks if the value matches either the title or by fields
+    else {
+      this.stories.forEach(story => {
+        if (
+          story.title
+            .toLowerCase()
+            .indexOf(event.target.value.toLowerCase()) !== -1 ||
+          story.by.toLowerCase().indexOf(event.target.value.toLowerCase()) !==
+            -1
+        ) {
+          temp.push(story);
+        }
+      });
+      this.filteredStories.next(temp);
     }
   }
 }
